3224a6f29c2fd1220fddd3c5e9c5855f8fc1293b
divyamttt <healthcultivators@gmail.com>
2025-07-17 22:57:31 -0400

    Create Header.jsx

---
+++ components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="py-6 text-center bg-white dark:bg-gray-900">
+      <h1 className="text-3xl font-bold text-blue-700">Faremee</h1>
+    </header>
+  );
+}
