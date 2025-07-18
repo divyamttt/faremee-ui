da0a81977509ca0de0f4127f3ec9bc488be4b4f7
divyamttt <healthcultivators@gmail.com>
2025-07-18 11:02:15 -0400

    Create SeatMap.jsx

---
+++ components/SeatMap.jsx
@@ -0,0 +1,7 @@
+export default function SeatMap() {
+  return (
+    <div className="bg-gray-200 dark:bg-gray-700 p-6 rounded-lg text-center">
+      Seat map selector coming soon…
+    </div>
+);
+}
