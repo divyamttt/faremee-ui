31b5ddee0bf3c29e114d94623755baf8a38f08f7
divyamttt <healthcultivators@gmail.com>
2025-07-18 11:18:10 -0400

    Create hotels.js

---
+++ pages/hotels.js
@@ -0,0 +1,17 @@
+import PageWrapper from "../components/PageWrapper";
+import Header from "../components/Header";
+import SearchBox from "../components/SearchBox";
+import Footer from "../components/Footer";
+
+export default function HotelsSearch() {
+  return (
+    <PageWrapper>
+      <Header />
+      <main className="py-12">
+        <h2 className="text-2xl font-bold mb-4">Search Hotels</h2>
+        <SearchBox service="hotels" />
+      </main>
+      <Footer />
+    </PageWrapper>
+  );
+}
