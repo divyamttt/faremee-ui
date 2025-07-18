c74394ca4c18ecc86a5dfc39ff096cb0c16ea4c1
divyamttt <healthcultivators@gmail.com>
2025-07-18 09:12:12 -0400

    Delete components directory

--- components/DealsCarousel.jsx
+++
@@ -1,9 +0,0 @@
-export default function DealsCarousel() {
-  return (
-    <div className="h-48 bg-gray-200 dark:bg-gray-700 flex items-center justify-center rounded-lg mb-8">
-      <span className="text-gray-600 dark:text-gray-300">
-        Deals Carousel Placeholder
-      </span>
-    </div>
-  );
-}
--- components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-6 text-center text-gray-600 dark:text-gray-400">
-      © {new Date().getFullYear()} Faremee
-    </footer>
-  );
-}
