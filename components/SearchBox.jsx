86d1c2a8f04f1450192c8cf1475c50d768acbdf7
divyamttt <healthcultivators@gmail.com>
2025-07-17 22:59:06 -0400

    Create SearchBox.jsx

---
+++ components/SearchBox.jsx
@@ -0,0 +1,9 @@
+export default function SearchBox() {
+  return (
+    <div className="p-6 bg-white dark:bg-gray-800 rounded-xl shadow-lg">
+      <p className="text-center text-gray-500 dark:text-gray-400">
+        Search Box Placeholder
+      </p>
+    </div>
+  );
+}
