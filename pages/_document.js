4dee289c7fa208b8130df55537b69045894f3429
divyamttt <healthcultivators@gmail.com>
2025-07-18 11:06:21 -0400

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const ip = await Document.getInitialProps(ctx);
+    return { ...ip };
+  }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        </Head>
+        <body className="bg-gray-50 dark:bg-gray-900">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
