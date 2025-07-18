3761a94ba7e201ab900f38fe12879573b0fce2e1
divyamttt <healthcultivators@gmail.com>
2025-07-18 09:12:59 -0400

    Create PageWrapper.jsx

---
+++ components/PageWrapper.jsx
@@ -0,0 +1,9 @@
+export default function PageWrapper({ children }) {
+  return (
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        {children}
+      </div>
+    </div>
+  );
+}
