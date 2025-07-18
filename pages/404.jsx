422f4271fb6c97d0cda7de29f35488f19509beb0
divyamttt <healthcultivators@gmail.com>
2025-07-18 09:12:25 -0400

    Delete pages directory

--- pages/404.jsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900">
-      <h1 className="text-4xl font-bold">404 — Page Not Found</h1>
-    </div>
-  );
-}
